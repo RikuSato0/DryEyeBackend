b5509941b9ab524d4b65def1d2130cb940f130c2
RikuSato0 <blockify41@gmail.com>
2025-10-03 01:43:10 +0900

    authentication

--- routes/product.routes.js
+++ routes/product.routes.js
@@ -3,14 +3,15 @@ const router = express.Router();
 const productController = require('../controllers/product.controller');
 const { uploadProductImage } = require('../middlewares/uploadProductImage');
 const adminOnly = require('../middlewares/adminOnly');
+const authenticationToken = require('../middlewares/authMiddleware');
 
-router.post('/add', adminOnly, uploadProductImage.single('image'), productController.add);
+router.post('/add', authenticationToken,adminOnly, uploadProductImage.single('image'), productController.add);
 router.post('/get', productController.list);
 router.post('/get-detail', productController.detail);
 router.post('/add-review', productController.addReview);
-router.post('/update', adminOnly, uploadProductImage.single('image'), productController.update);
-router.post('/delete', adminOnly, productController.delete);
-router.post('/get-all', adminOnly, productController.getAll);
+router.post('/update', authenticationToken,adminOnly, uploadProductImage.single('image'), productController.update);
+router.post('/delete', authenticationToken,adminOnly, productController.delete);
+router.post('/get-all', authenticationToken,adminOnly, productController.getAll);
 
 module.exports = router;
 
